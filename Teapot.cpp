a51f2063aa3058054d9ece41155d238223c10b01
Patrick McGuire <pjm@Patrick-McGuires-MacBook-Pro.local>
2011-10-10 13:20:26 -0400

    OS X is a drag, can't find the proper files

---
+++ Teapot.cpp
@@ -0,0 +1,21 @@
+/*
+ * Teapot.cpp
+ *
+ *  Created on: Oct 10, 2011
+ *      Author: pjm
+ */
+
+#include "Teapot.h"
+
+Teapot::Teapot() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Teapot::~Teapot() {
+	// TODO Auto-generated destructor stub
+}
+
+void Teapot::draw() {
+
+}
---
+++ Teapot.h
@@ -0,0 +1,18 @@
+/*
+ * Teapot.h
+ *
+ *  Created on: Oct 10, 2011
+ *      Author: pjm
+ */
+
+#ifndef TEAPOT_H_
+#define TEAPOT_H_
+
+class Teapot : Node {
+public:
+	void draw();
+	Teapot();
+	virtual ~Teapot();
+};
+
+#endif /* TEAPOT_H_ */
