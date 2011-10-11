c17417ec21881619f46b674be278271992495109
Patrick McGuire <pjm2119@columbia.edu>
2011-10-10 23:18:53 -0400

    oh no

--- main.cpp
+++ main.cpp
@@ -2,13 +2,7 @@
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/glext.h>
-#include <stddef.h>
-
-#include "Teapot.h"
-#include "Floor.h"
-#include "Box.h"
-#include "Torus.h"
-#include "Sphere.h"
+#include <iostream>
 
 const int WIDTH = 600;
 const int HEIGHT = 480;
@@ -35,27 +29,32 @@ void init() {
 
 void display() {
 	glClear( GL_COLOR_BUFFER_BIT );
-	glutSwapBuffers();
 
 	int leftmost = -15;
 	int rightmost = 15;
 	int closest = -10;
 	int furthest = 10;
 
-	glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
-	glColor3i(128, 128, 128);
+	glPolygonMode(GL_FRONT,GL_LINE);
+	glColor3f(0.5f, 0.5f, 0.5f);
+
+	int iinc = (rightmost - leftmost) / 15;
+	int jinc = (furthest - closest) / 10;
+	int j = 0;
+
 	glBegin(GL_QUAD_STRIP);
-		for(int i = leftmost; i < rightmost; i += ((rightmost - leftmost) / 15)) {
-			for(int j = closest; j < furthest; j += ((furthest - closest) / 10)) {
-				glVertex3i(i,0,j);
-				if(i < rightmost - 1) {
-					glVertex3i(i+1,0,j);
-				}
-			}
-		}
+	glPolygonMode(GL_FRONT,GL_LINE);
+		glVertex2i(leftmost + iinc, closest);
+		glVertex2i(leftmost, closest);
+		glVertex2i(leftmost, closest + iinc);
+		glVertex2i(leftmost + iinc, closest + iinc);
 	glEnd();
 
-	gluLookAt(2, 4, 2,
-			 0, 0, 0,
-			 0, 1, 0);
+
+	glPolygonMode(GL_FRONT,GL_FILL);
+
+
+	glutSwapBuffers();
+
+
 }
