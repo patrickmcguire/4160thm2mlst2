30bc931b7655b6ea6d1d38114018eb13b86f6d92
Patrick McGuire <pjm2119@columbia.edu>
2011-10-10 22:18:09 -0400

    I've been barking up the wrong tree.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,61 @@
+#include <GL/glut.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glext.h>
+#include <stddef.h>
+
+#include "Teapot.h"
 #include "Floor.h"
+#include "Box.h"
+#include "Torus.h"
+#include "Sphere.h"
 
-int main( int argc, const char* argv[] ) {
+const int WIDTH = 600;
+const int HEIGHT = 480;
 
-	Node * f  = new Floor();
+void init();
+void display();
 
+int main( int argc, char *argv[] ) {
+
+	glutInit(&argc, argv);
+	glutInitDisplayMode( GLUT_DOUBLE | GLUT_RGB );
+	glutInitWindowPosition( 100, 100 );
+	glutInitWindowSize( WIDTH, HEIGHT );
+	glutCreateWindow( argv[0] );
+	init();
+	glutDisplayFunc(display);
+	glutMainLoop();
 	return 0;
+}
+
+void init() {
+	glClearColor( 0.0, 0.0, 0.0, 1.0 );
+}
+
+void display() {
+	glClear( GL_COLOR_BUFFER_BIT );
+	glutSwapBuffers();
+
+	int leftmost = -15;
+	int rightmost = 15;
+	int closest = -10;
+	int furthest = 10;
+
+	glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
+	glColor3i(128, 128, 128);
+	glBegin(GL_QUAD_STRIP);
+		for(int i = leftmost; i < rightmost; i += ((rightmost - leftmost) / 15)) {
+			for(int j = closest; j < furthest; j += ((furthest - closest) / 10)) {
+				glVertex3i(i,0,j);
+				if(i < rightmost - 1) {
+					glVertex3i(i+1,0,j);
+				}
+			}
+		}
+	glEnd();
+
+	gluLookAt(2, 4, 2,
+			 0, 0, 0,
+			 0, 1, 0);
 }
--- makefile
+++ makefile
@@ -1,18 +1,30 @@
-CFLAGS = -Wall
+CFLAGS = -g -O0 -Wall -I/usr/include/GL/ -lGL -lglut -lGLU
 
 all: program
 
-program: Node.o Floor.o main.o
-	g++ $(CFLAGS) -o program main.o Floor.o Node.o
+program: Node.o Floor.o main.o Box.o Teapot.o Torus.o Sphere.o
+	g++ $(CFLAGS) -o program main.o Floor.o Box.o Teapot.o Torus.o Sphere.o Node.o
 
 main.o: main.cpp Node.h Floor.h
-	g++ $(CFLAGS) -c main.cpp Node.h Floor.h
+	g++ $(CFLAGS) -c main.cpp
 
 Floor.o: Floor.cpp Node.h Floor.h
-	g++ $(CFLAGS) -c Floor.cpp Floor.h Node.h
+	g++ $(CFLAGS) -c Floor.cpp
 
 Node.o: Node.h Node.cpp
-	g++ $(CFLAGS) -c Node.cpp Node.h 
+	g++ $(CFLAGS) -c Node.cpp
+	
+Box.o: Box.cpp Node.h Box.h
+	g++ $(CFLAGS) -c Box.cpp
+
+Teapot.o: Teapot.cpp Teapot.h Node.h
+	g++ $(CFLAGS) -c Teapot.cpp
+	
+Torus.o: Torus.cpp Torus.h Node.h
+	g++ $(CFLAGS) -c Torus.cpp
+	
+Sphere.o: Sphere.cpp Sphere.h Node.h
+	g++ $(CFLAGS) -c Sphere.cpp
 	
 clean:
 	rm *.o *.gch program
